c52715785a24d59b0db4ed5b428de419fe3dd3ae
RazorzMcLazorz <nateyoungdev@gmail.com>
2023-04-15 00:37:54 -0600

    font style added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import mountainsSunset from '@/assets/mountainsSunset.svg'
 import Landing from '@/pages/Landing/Landing'
 import Navbar from '@/components/Navbar/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
